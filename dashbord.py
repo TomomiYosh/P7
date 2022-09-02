3339eabbf29b762fa9b6e64e5a2d5ba9a8e9662b
Tomomi <tomomi@biadala.net>
2022-09-02 12:23:50 +0200

    Changer quelques commentaires dans les codes

--- dashbord.py
+++ dashbord.py
@@ -1,37 +1,20 @@
 # http://127.0.0.1:5000/
-# Streamlit
-# Le tableau de bord intégrera au minimum :
-# N° client, crédit accepté ou non, score détaillé sous forme de jauge : permet de juger s’il est loin du seuil ou non.
-# Sa feature importance locale sous forme de graphique, qui permet au chargé d’étude de comprendre quelles sont les données du client qui ont le plus influencé le calcul de son score
-# Le tableau de bord présentera également d’autres graphiques sur les autres clients :
-# Deux graphiques de features sélectionnées dans une liste déroulante, présentant la distribution de la feature selon les classes, ainsi que le positionnement de la valeur du client
-# Un graphique d’analyse bi-variée entre les deux features sélectionnées, avec un dégradé de couleur selon le score des clients, et le positionnement du client
-# La feature importance globale
-# D’autres graphiques complémentaires
-# https://blog.streamlit.io/how-to-build-a-real-time-live-dashboard-with-streamlit/
+# Dashboard
 
-# dans pd.read_csv le paramètre "nrows" limite le nombre de lignes chargée en mémoire,
-# a supprimer avant de rendre le projet.
-
-import time  # to simulate a real time data, time loop
-import numpy as np  # np mean, np random
-import pandas as pd  # read csv, df manipulation
-import plotly.express as px  # interactive charts
+import pandas as pd
 import streamlit as st  # 🎈 data web app development
 import requests
 import json
 from zipfile import ZipFile
 
-#app = st(__name__)
-
-# Créatuib de page
+# Création de page
 st.set_page_config(
     page_title="Credit Scoring Dashboard",
     page_icon="🎈",
     layout="wide",
 )
 
-# dashboard title
+# Titre de dashboard
 st.title("Informations client")
 
 
@@ -40,29 +23,28 @@ def load_prediction(client_id):
     client_data = requests.get(f'http://127.0.0.1:5000/predict/{client_id}')
     return json.loads(client_data.content)
 
-# text input
-#Customer ID selection
+
+# Selection d'un ID crédit
 st.sidebar.header("**Credit scoring dashboard**")
-#Loading selectbox
+
+# Selectbox
 client_id = st.sidebar.text_input("Mettez un ID crédit")
 
+# Explication des features
 st.sidebar.subheader("**Description d'une feature**")
 description = pd.read_csv("HomeCredit_columns_description.csv", index_col='Row', sep=",")
 choix = st.sidebar.selectbox('Choisissez une feature que vous voulez avoir une explication', description.index)
 explication = description.loc[choix]
-# if len(explication['Description']) > 1:
-#    st.sidebar.write("Description : ", explication['Description'])
 st.sidebar.write("Description : ", explication['Description'])
 
+# Information du score et résultat des études
 if client_id:
     client_id = int(client_id)
-    # st.write("Client ID : ", client_id)
     st.subheader("**Information de score du client** {:.0f}".format(client_id))
-    # {:.0f} ans".format(int(client_infos["DAYS_BIRTH"]/-365.25))
     client_prediction = load_prediction(client_id)
     st.write("Résultat des études dossier : ", client_prediction["Résultat des études"])
 
-
+    # Gauge
     import plotly.graph_objects as go
     import matplotlib.pyplot as plt
 
@@ -85,17 +67,16 @@ if client_id:
 
     st.plotly_chart(fig)
 
+    # Load le modèle entrainé
     import pickle
-    # loading the trained model
     pickle_in = open('model_final', 'rb')
     clf = pickle.load(pickle_in)
 
-    # Feature importance / description
+    # Feature importance locale
     # Données X standardisées
     z = ZipFile("data.zip")
     main_data = pd.read_csv(z.open('X.csv'), index_col='SK_ID_CURR', sep=",", encoding='utf-8', nrows=100)
     main_data = main_data.drop({'Unnamed: 0'}, axis=1)
-
 
     import shap
     if st.checkbox("Afficher feature importance de ce client ?"):
@@ -105,13 +86,11 @@ if client_id:
         fig, ax = plt.subplots(figsize=(10, 20))
         explainer = shap.TreeExplainer(clf)
         shap_values = explainer.shap_values(X)
-        # shap.summary_plot(shap_values, features=X, feature_names=X.columns)
         shap.summary_plot(shap_values, X, plot_type="bar", max_display=20, color_bar=False, plot_size=(5, 5))
-        # shap.force_plot(explainer.expected_value[0], shap_values[0], X[0], feature_names=X.columns)
         st.pyplot(fig)
         X = False
 
-    #Customer information display : Customer Gender, Age, Family status, Children, …
+    # Informations détaillées d'un client
     st.subheader("**Informations détailées du client**")
 
     if st.checkbox("Plus d'informations clients ?"):
@@ -134,7 +113,7 @@ if client_id:
         st.write("Nombre d'années employé : {:.0f} ans".format(int(client_infos["DAYS_EMPLOYED"]/-365.25)))
         st.write("Numbre d'enfants : {:.0f}".format(client_infos["CNT_CHILDREN"]))
 
-    # Sur les autres clients
+    # Infos sur les autres clients
     # La feature importance globale
     st.header("**Informations des autres clients**")
     shap.initjs()
@@ -146,31 +125,26 @@ if client_id:
         explainer = shap.TreeExplainer(clf)
         shap_values_m = explainer.shap_values(main_data)
         shap.summary_plot(shap_values_m, features=main_data, feature_names=main_data.columns, plot_type='bar')
-        # shap.summary_plot(shap_values_m, main_data, plot_type="bar", max_display=20, color_bar=False, plot_size=(5, 5))
-        # shap.force_plot(explainer.expected_value[0], shap_values[0], X[0], feature_names=X.columns)
         st.pyplot(fig)
 
     with col2:
         # Distribution de features sélectionnées
         st.subheader("**Distribution de la feature sélectionnée**")
 
-        # Données non stardadisées
+        # Données non standardisées
         data = pd.read_csv(z.open("data.csv"), index_col='SK_ID_CURR', sep=",", nrows=100)
-        #data_1 = data.query('TARGET == 1.0')
-        #data_0 = data.query('TARGET == 0.0')
         data = data.drop({'Unnamed: 0'}, axis=1)
         data2 = data.drop({'TARGET'}, axis=1)
         data_X = data.loc[client_id]
 
-        # Add selectbox in streamlit
+        # Selectbox et graphique box plot
         import seaborn as sns
         option = st.selectbox('Choisissez une feature que vous souhaitez étudier', data2.columns)
         st.write("Le point rouge concerne le client du crédit concerné.")
         if option:
-            #plt.title(f"Distribution de la classe 0 (crédit accepté) de la feature {option}")
             g = sns.catplot(x="TARGET", y=option, kind="box", data=data)
 
-            # adding data points
+            # Ajouter un point rouge du client concerné
             g.ax.scatter(x=data_X["TARGET"], y=data_X[option],c="red",zorder=10)
             sns.set(rc={'figure.figsize':(6,4)})
             st.pyplot(g)
@@ -178,7 +152,7 @@ if client_id:
     col4, col5 = st.columns(2)
 
     with col4:
-        # Un graphique d’analyse bi-variée entre les deux features sélectionnées, avec un dégradé de couleur selon le score des clients, et le positionnement du client
+        # Un graphique d’analyse bi-variée entre les deux features sélectionnées
         st.subheader("**Analyse bi-variée entre les deux features sélectionnées**")
         st.write("Le point rouge concerne le client du crédit concerné. Si le couleur est plus foncé, le score est plus élevé.")
         option2 = st.selectbox('Choisissez la première feature (abscisse) que vous souhaitez étudier', data2.columns)
@@ -188,7 +162,6 @@ if client_id:
             fig = plt.figure(figsize=(6, 6))
             plt.scatter(x=data[option2], y=data[option3], c=score, cmap='YlGnBu')
             plt.scatter(x=data_X[option2], y=data_X[option3], c="red", zorder=10)
-            # sns.set(rc={'figure.figsize': (8, 6)})
             st.pyplot(fig)
 
 
--- scoringapi.py
+++ scoringapi.py
@@ -1,22 +1,21 @@
-# http://127.0.0.1:5000/predict/id
-# import main Flask class and request object
+# http://127.0.0.1:5000/
+# API
 
 import pandas as pd
 from flask import Flask, request, jsonify
 import pickle
 from zipfile import ZipFile
 
-# create the Flask app: obtain the API server
+# Création d'application Flask app: obtenir l'API server
 app = Flask(__name__)
 
-# loading the trained model
+# Load le modèle entrainé
 pickle_in = open('model_final', 'rb')
 clf = pickle.load(pickle_in)
 
-# Données X standardisées
+# Charger les données X standardisées
 z = ZipFile("data.zip")
 X = pd.read_csv(z.open('X.csv'), index_col='SK_ID_CURR', sep=",", encoding='utf-8')
-# X = pd.read_csv("X.csv", index_col='SK_ID_CURR', sep=",")
 X = X.drop({'Unnamed: 0'}, axis=1)
 
 # Données non stardadisées
@@ -25,18 +24,14 @@ data = pd.read_csv(z.open('data.csv'), index_col='SK_ID_CURR', sep=",", encoding
     "DAYS_EMPLOYED",
     "CNT_CHILDREN","AMT_INCOME_TOTAL","AMT_CREDIT"
 ])
-# data = pd.read_csv("data.csv", index_col='SK_ID_CURR', sep=",", usecols=[
-#    "SK_ID_CURR", "CODE_GENDER", "DAYS_BIRTH",
-#    "DAYS_EMPLOYED",
-#    "CNT_CHILDREN","AMT_INCOME_TOTAL","AMT_CREDIT"
-# ])
+
 
 @app.route('/') #Créer Accueil
 def home():
     return 'Home'
 
 
-# API qui envoie le score, résultat et shap values des clients
+# API qui envoie le score, résultat du client choisi
 @app.route('/predict/<int:client_id>', methods=['GET', 'POST'])
 def query_score(client_id):
     score = False
@@ -55,13 +50,11 @@ def query_score(client_id):
         "Résultat des études": resultat})
 
 
-# Afficher les infos clients selon id
-@app.route('/client/<int:client_id>') # Url / Route / API
-def client_info(client_id):  # Paramètre
+# API qui envoie des infos clients selon id
+@app.route('/client/<int:client_id>')
+def client_info(client_id):
     if client_id in data.index:
-        #client = data.query(f'data.index == {client_id}') # Pour que la fonction comprenne que 'client_id' est une variable
         client = data.loc[client_id]
-        # res = client.to_json()
         return jsonify({
             "CODE_GENDER": client['CODE_GENDER'],
             "DAYS_BIRTH": client['DAYS_BIRTH'],
@@ -69,13 +62,8 @@ def client_info(client_id):  # Paramètre
             "CNT_CHILDREN": client['CNT_CHILDREN'],
             "AMT_INCOME_TOTAL": client['AMT_INCOME_TOTAL'],
             "AMT_CREDIT": client['AMT_CREDIT']})
-    # res # Return doit être toujours en string
-
-# if key doesn't exist, returns None
-    #client_id = request.args.get('client_id')
-    # filtre sur data lorsque id=client_id
 
 if __name__ == '__main__':
-    # run app in debug mode on port 5000
+    # Marcher l'app dans debug mode sur le port 5000
     app.run(debug=True, port=5000) # Lance le server
 
