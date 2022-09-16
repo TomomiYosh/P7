b08afe4a28f2ab40be8926a8f589cbe862bb5aac
TomomiYosh <kittywithtomomi@gmail.com>
2022-09-16 10:09:17 +0200

    Correction des erreurs de l'explication des features

--- dashbord.py
+++ dashbord.py
@@ -36,7 +36,7 @@ client_id = st.sidebar.text_input("Entrez un ID crédit")
 st.sidebar.subheader("**Description d'une feature**")
 description = pd.read_csv("HomeCredit_columns_description.csv", sep=",")
 description = description.query('Row != SK_ID_CURR')
-choix = st.sidebar.selectbox('Choisissez une feature que vous voulez avoir une explication', description.Row)
+choix = st.sidebar.selectbox('Choisissez une feature que vous voulez avoir une explication', description['Row'])
 explication = description.loc[choix]
 st.sidebar.write("Description : ", explication['Description'])
 
